55545ecdc7a0bf9b5404038912653beb26e8e440
Ostanin Dmitry <ostaninvlk@gmail.com>
2018-10-28 21:58:57 +1000

    Commit1

--- src/ostanin/tasks/Task14.java
+++ src/ostanin/tasks/Task14.java
@@ -1,10 +1,18 @@
 package ostanin.tasks;
 
-import java.util.Scanner
+import java.util.Scanner;
 
 public class Task14 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("")
+        int x, y, z;
+        System.out.println("введите x:");
+        x = in.nextInt();
+        System.out.println("введите y:");
+        y = in.nextInt();
+        System.out.println("введите z:");
+        z = in.nextInt();
+        double sum = (x + y + z)/3;
+        System.out.println("Среднеарифметическое суммы чисел x, y, z = " + "" + sum);
     }
 }
